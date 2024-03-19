0512fa3f5d2eba81635848f9d665602ec780e0e1
Caleb Cushing <xenoterracide@gmail.com>
2024-03-19 01:59:06 -0400

    Revert "build: spotless"
    This reverts commit 349fd93343d5c42502f69c655809bdc84bea8b10.

--- buildSrc/src/main/kotlin/our.spotless.gradle.kts
+++ buildSrc/src/main/kotlin/our.spotless.gradle.kts
@@ -20,7 +20,7 @@ spotless {
   }
 
   kotlinGradle {
-    target("*.gradle.kts", "buildSrc/**/*.gradle.kts", "module/*/*.gradle.kts")
+    target("*.gradle.kts", "buildSrc/*.gradle.kts", "buildSrc/**/*.gradle.kts", "modules/*/*.gradle.kts")
     targetExclude("build", "buildSrc/build", "modules/*/build")
     ktlint().editorConfigOverride(mapOf("ktlint_standard_value-argument-comment" to "disabled"))
     licenseHeader(copyright + gradleLicense, "(import|buildscript|plugins|root)")
