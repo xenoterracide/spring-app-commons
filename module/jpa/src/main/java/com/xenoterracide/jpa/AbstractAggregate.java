19b84f49aa71a353c68ae1456a3635d0c2de76d2
Caleb Cushing <xenoterracide@gmail.com>
2024-03-19 02:00:06 -0400

    feat: remove ddd code for this branch

--- module/jpa/src/main/java/com/xenoterracide/jpa/AbstractAggregate.java
+++ module/jpa/src/main/java/com/xenoterracide/jpa/AbstractAggregate.java
@@ -5,14 +5,7 @@ package com.xenoterracide.jpa;
 
 import jakarta.persistence.MappedSuperclass;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import org.jspecify.annotations.NonNull;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.domain.AfterDomainEventPublication;
-import org.springframework.data.domain.DomainEvents;
 
 /**
  * An abstract class for Domain Aggregates.
@@ -22,8 +15,6 @@ import org.springframework.data.domain.DomainEvents;
 @MappedSuperclass
 public abstract class AbstractAggregate<ID extends AbstractIdentity<? extends Serializable>>
   extends AbstractEntity<ID> {
-
-  private final transient @Transient List<Object> domainEvents = new ArrayList<>();
 
   /**
    * NO-OP parent constuctor for JPA only.
@@ -37,36 +28,5 @@ public abstract class AbstractAggregate<ID extends AbstractIdentity<? extends Se
    */
   protected AbstractAggregate(@NonNull ID id) {
     super(id);
-  }
-
-  /**
-   * Registers the given event object for publication on a call to a Spring Data repository's save or delete methods.
-   *
-   * @param <T>   the type of event
-   * @param event must not be {@literal null}.
-   * @return the event that has been added.
-   */
-  protected <T> @NonNull T registerEvent(@NonNull T event) {
-    this.domainEvents.add(event);
-    return event;
-  }
-
-  /**
-   * Clears all domain events currently held. Usually invoked by the infrastructure in place in Spring Data
-   * repositories.
-   */
-  @AfterDomainEventPublication
-  protected void clearDomainEvents() {
-    this.domainEvents.clear();
-  }
-
-  /**
-   * All domain events currently captured by the aggregate.
-   *
-   * @return the collection
-   */
-  @DomainEvents
-  protected Collection<Object> domainEvents() {
-    return Collections.unmodifiableList(this.domainEvents);
   }
 }
