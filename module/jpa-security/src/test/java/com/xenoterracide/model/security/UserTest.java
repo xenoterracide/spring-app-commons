8307304fb48423accb832cc05eb7f7ee8b18eb80
Caleb Cushing <xenoterracide@gmail.com>
2024-03-28 05:56:09 -0400

    chore: split test

--- module/jpa-security/src/test/java/com/xenoterracide/model/security/UserTest.java
+++ module/jpa-security/src/test/java/com/xenoterracide/model/security/UserTest.java
@@ -3,39 +3,11 @@
 
 package com.xenoterracide.model.security;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import jakarta.persistence.EntityManager;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DataJpaTest
-class UserTest {
-
-  @Autowired
-  UserRepository userRepository;
-
-  @Autowired
-  EntityManager em;
-
-  @Test
-  void save() {
-    var u0 = UserTestDataBuilder.create().build();
-    userRepository.save(u0);
-    em.flush();
-    em.clear();
-
-    var u1 = userRepository.findById(u0.getId());
-    assertThat(u1).isPresent();
-
-    var u2 = userRepository.findByIdentityProviderUser(u0.getIdentityProviderUsers().iterator().next().getId());
-    assertThat(u2).isPresent();
-
-    assertThat(u2.get()).isEqualTo(u1.get());
-  }
+public class UserTest {
 
   @Test
   void equality() {
