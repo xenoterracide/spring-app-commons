8ace08c516fc0a42feee117cceaf5abcc6ec5473
Caleb Cushing <xenoterracide@gmail.com>
2024-04-02 04:15:35 -0400

    chore: remove unused configuration

--- module/test-app/build.gradle.kts
+++ module/test-app/build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
   implementation(platform(libs.spring.bom))
   implementation(libs.spring.boot.autoconfigure)
   implementation(libs.spring.test)
-  implementation(libs.spring.boot.test.autoconfigure)
 
   runtimeOnly(platform(libs.spring.bom))
   runtimeOnly(libs.starter.log4j2)
