2721e9c64c3b3ae65e933af343511db8c3b1016c
Caleb Cushing <xenoterracide@gmail.com>
2024-04-04 07:28:34 -0400

    chore: remove -w error

--- module/jpa/build.gradle.kts
+++ module/jpa/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
 
 tasks.compileJava {
   options.release = 17
-  options.compilerArgs.removeIf { it == "-Werror" } // either that or remove specific jpa modelgen warnings
   options.compilerArgs.addAll(
     listOf(
       "-AaddSuppressWarningsAnnotation=true",
--- module/model-security/build.gradle.kts
+++ module/model-security/build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
 
 tasks.compileJava {
   options.release = 17
-  options.compilerArgs.removeIf { it == "-Werror" } // either that or remove specific jpa modelgen warnings
   options.compilerArgs.addAll(
     listOf(
       "-AaddSuppressWarningsAnnotation=true",
