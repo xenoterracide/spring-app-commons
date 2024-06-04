f0227b16103bc502cb9cef4a221396aaf46acfb3
Caleb Cushing <xenoterracide@gmail.com>
2024-06-04 12:57:04 -0400

    chore: downgrade jpamodelgen

--- module/model-security/build.gradle.kts
+++ module/model-security/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
   annotationProcessor(platform(libs.spring.bom))
   annotationProcessor(platform(libs.immutables.bom))
   annotationProcessor(libs.immutables.core)
-  annotationProcessor(libs.hibernate.jpa.modelgen)
+  // annotationProcessor(libs.hibernate.jpa.modelgen)
 
   compileOnly(platform(libs.immutables.bom))
   compileOnly(libs.bundles.immutables)
