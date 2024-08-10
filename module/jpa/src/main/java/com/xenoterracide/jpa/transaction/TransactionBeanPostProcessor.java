80bf4458ca8f2323d1282f8d9b5008b18ea91758
Caleb Cushing <xenoterracide@gmail.com>
2024-08-10 11:05:10 -0400

    feat: fix package structure

--- module/jpa/src/main/java/com/xenoterracide/jpa/transaction/TransactionBeanPostProcessor.java
+++ module/jpa/src/main/java/com/xenoterracide/jpa/transaction/TransactionBeanPostProcessor.java
@@ -1,7 +1,7 @@
 // © Copyright 2024 Caleb Cushing
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-package com.xenoterracide.jpa.test.transaction;
+package com.xenoterracide.jpa.transaction;
 
 import com.xenoterracide.jpa.annotation.TransactionScope;
 import com.xenoterracide.jpa.util.Constants;
