e758790b382470ff2e5f689461a7ef6abc24194b
Caleb Cushing <xenoterracide@gmail.com>
2024-09-13 15:49:21 -0400

    test: treat jars as binary

--- module/controller-registration/src/main/java/module-info.java
+++ module/controller-registration/src/main/java/module-info.java
@@ -2,9 +2,7 @@
  * This module provides the registration controller.
  */
 module com.xenoterracide.controller.registration {
-  // exports com.xenoterracide.controller.registration to spring.beans, spring.context; // maybe not needed
-  opens com.xenoterracide.controller.registration
-    to spring.core, org.hibernate.validator, com.graphqljava, spring.graphql;
+  opens com.xenoterracide.controller.registration to spring.core, org.hibernate.validator;
   requires spring.graphql;
   requires spring.context;
   requires com.xenoterracide.model.security;
