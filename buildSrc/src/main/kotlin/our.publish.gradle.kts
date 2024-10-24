ec43de6166647038b1c4923e3ca2725c3b2d084f
Caleb Cushing <caleb.cushing@gmail.com>
2024-10-24 08:16:58 -0400

    build: use my new publish plugin (#165)

--- buildSrc/src/main/kotlin/our.publish.gradle.kts
+++ buildSrc/src/main/kotlin/our.publish.gradle.kts
@@ -4,93 +4,18 @@
 plugins {
   `maven-publish`
   id("com.xenoterracide.gradle.semver")
+  id("com.xenoterracide.gradle.convention.publish")
 }
 
-val repo = "spring-app-commons"
-val username = "xenoterracide"
-val githubUrl = "https://github.com"
-val repoShort = "$username/$repo"
+publicationLegal {
+  this.inceptionYear.set(2024)
+  this.spdxLicenseIdentifiers.addAll("AGPL-3.0-or-later", "Universal-FOSS-exception-1.0", "CC-BY-NC-4.0")
+}
 
 publishing {
   publications {
     register<MavenPublication>("maven") {
       from(components["java"])
-    }
-    withType<MavenPublication>().configureEach {
-      versionMapping {
-        allVariants {
-          fromResolutionResult()
-        }
-      }
-      pom {
-        artifactId = project.name
-        groupId = rootProject.group.toString()
-        version = semver.gitDescribed.version
-        description = project.description
-        inceptionYear = "2024"
-        url = "$githubUrl/$repoShort"
-        licenses {
-          license {
-            name = "AGPL-3.0-or-later"
-            url = "https://choosealicense.com/licenses/agpl-3.0/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "Universal-FOSS-exception-1.0"
-            url = "https://spdx.org/licenses/Universal-FOSS-exception-1.0.html"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "MIT"
-            url = "https://choosealicense.com/licenses/mit/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "CC-BY-4.0"
-            url = "https://choosealicense.com/licenses/cc-by-4.0/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-          license {
-            name = "CC-BY-NC-4.0"
-            url = "https://creativecommons.org/licenses/by-nc/4.0/"
-            distribution = "repo"
-            comments = "See README.md for more information."
-          }
-        }
-        developers {
-          developer {
-            id = username
-            name = "Caleb Cushing"
-            email = "xenoterracide@gmail.com"
-          }
-        }
-        scm {
-          connection = "$githubUrl/$repoShort.git"
-          developerConnection = "scm:git:$githubUrl/$repoShort.git"
-          url = "$githubUrl/$repoShort"
-        }
-      }
-    }
-  }
-
-  repositories {
-    maven {
-      name = "gh"
-      url = uri("https://maven.pkg.github.com/$repoShort")
-      credentials {
-        // use properties because gradles credentials errors if missing
-        providers.gradleProperty("ghUsername").let { username = it.orNull }
-        providers.gradleProperty("ghPassword").let { password = it.orNull }
-        // avoid congiguration cache missing on credentials
-        if (username == null || password == null) {
-          username = System.getenv("GITHUB_ACTOR")
-          password = System.getenv("GITHUB_TOKEN")
-        }
-      }
     }
   }
 }
