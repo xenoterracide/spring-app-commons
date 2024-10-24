6d697165136066c9beebfa448037ed1c02fa1b95
Caleb Cushing <caleb.cushing@gmail.com>
2024-10-24 08:08:00 -0400

    build: use my new publish plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,7 @@ dependencyLocking { lockAllConfigurations() }
 dependencies {
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
   implementation(libs.plugin.convention.coverage)
+  implementation(libs.plugin.convention.publish)
   implementation(libs.plugin.spotless)
   implementation(libs.plugin.spotbugs)
   implementation(libs.plugin.errorprone)
