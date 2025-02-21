ead06a0703b79afeb9162ae9e697ada28c7c8172
Caleb Cushing <caleb.cushing@gmail.com>
2025-02-21 15:40:07 -0500

    chore: update java

--- module/security-controller/src/main/java/com/xenoterracide/controller/security/package-info.java
+++ module/security-controller/src/main/java/com/xenoterracide/controller/security/package-info.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Copyright © 2024 Caleb Cushing
+//
+// SPDX-License-Identifier: (AGPL-3.0-or-later WITH Universal-FOSS-exception-1.0 AND CC-BY-4.0) OR CC-BY-NC-4.0
+
 /**
  * Registration controller.
  */
--- module/test-authorization-server/src/test/java/com/xenoterracide/authorization/server/AuthorizationServerTest.java
+++ module/test-authorization-server/src/test/java/com/xenoterracide/authorization/server/AuthorizationServerTest.java
@@ -1,5 +1,6 @@
-// © Copyright 2024 Caleb Cushing
-// SPDX-License-Identifier: AGPL-3.0-or-later WITH Universal-FOSS-exception-1.0 AND CC-BY-4.0 OR CC-BY-NC-4.0
+// Copyright 2024 Caleb Cushing
+//
+// SPDX-License-Identifier: (AGPL-3.0-or-later WITH Universal-FOSS-exception-1.0 AND CC-BY-4.0) OR CC-BY-NC-4.0
 
 package com.xenoterracide.authorization.server;
 
