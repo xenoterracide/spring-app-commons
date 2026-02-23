f5947dea66e7aa8de3051be2483e80d38669fd92
Caleb Cushing <caleb.cushing@gmail.com>
2026-02-23 07:06:20 -0500

    chore(gradle): remove unused `sbd3` version catalog configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -88,12 +88,6 @@ rootDir.resolve("module").listFiles()?.forEach { file ->
 
 dependencyResolutionManagement {
   versionCatalogs {
-    create("sbd3") {
-      from("com.xenoterracide.gradle.vc:version-catalog-spring-boot:3.5.0")
-      bundle("spring-test", listOf("spring-test", "spring-boot-test", "spring-boot-test-autoconfigure"))
-      bundle("test-impl", listOf("assertj-core", "junit-jupiter-api", "junit-jupiter-params"))
-      bundle("test-runtime", listOf("junit-jupiter-engine", "junit-platform-launcher"))
-    }
     create("sbd4") {
       from("com.xenoterracide.gradle.vc:version-catalog-spring-boot:4.0.0")
       bundle("spring-test", listOf("spring-test", "spring-boot-test", "spring-boot-test-autoconfigure"))
