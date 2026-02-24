8c5ba4b9e2f6151fa584fc034bb4dd171b43b7ad
Caleb Cushing <caleb.cushing@gmail.com>
2026-02-24 12:00:06 -0500

    chore: update copyrights

--- module/commons-model/src/test/java/module-info.java
+++ module/commons-model/src/test/java/module-info.java
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: Copyright © 2024, 2026 Caleb Cushing
 //
-// SPDX-License-Identifier: (AGPL-3.0-or-later WITH Universal-FOSS-exception-1.0 AND CC-BY-4.0) OR CC-BY-NC-4.0
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import org.jspecify.annotations.NullMarked;
@@ -16,7 +15,6 @@ import org.jspecify.annotations.NullMarked;
   requires org.junit.jupiter.api;
   requires org.assertj.core;
   requires com.xenoterracide.commons.model;
-  // requires spring.boot.test.autoconfigure;
   requires spring.beans;
   requires jakarta.persistence;
   requires org.jmolecules.ddd;
--- module/security-model/src/test/java/module-info.java
+++ module/security-model/src/test/java/module-info.java
@@ -1,6 +1,7 @@
-// SPDX-FileCopyrightText: Copyright © 2024 - 2025 Caleb Cushing
+// SPDX-FileCopyrightText: Copyright © 2024-2026 Caleb Cushing
 //
 // SPDX-License-Identifier: (AGPL-3.0-or-later WITH Universal-FOSS-exception-1.0 AND CC-BY-4.0) OR CC-BY-NC-4.0
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 import org.jspecify.annotations.NullMarked;
 
@@ -21,4 +22,5 @@ import org.jspecify.annotations.NullMarked;
   requires spring.orm;
   requires com.xenoterracide.model.security;
   requires jakarta.persistence;
+  requires spring.boot.data.jpa.test;
 }
